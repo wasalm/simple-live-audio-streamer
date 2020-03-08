1758e07064975dcbb72c0a6f5ec20827f041a17d
Andries Salm <info@andries-salm.com>
2020-03-08 17:04:42 +0000

    Update settings screen

--- experiments/app-interface/v2/assets/scripts/settings.js
+++ experiments/app-interface/v2/assets/scripts/settings.js
@@ -1,10 +1,13 @@
 document.getElementById("audio-channels").addEventListener("change", updateSettingsScreen);
+document.getElementById("audio-volume").addEventListener("change", updateSettingsScreen);
 document.getElementById("proxy").addEventListener("change", updateSettingsScreen);
 
 
 function updateSettingsScreen() {
 	document.getElementById("audio-channels-left").disabled = !document.getElementById("audio-channels").checked;
 	document.getElementById("audio-channels-right").disabled = !document.getElementById("audio-channels").checked;
+
+	document.getElementById("audio-volume-val").disabled = !document.getElementById("audio-volume").checked;
 
 	document.getElementById("proxy-user").disabled = !document.getElementById("proxy").checked;
 	document.getElementById("proxy-host").disabled = !document.getElementById("proxy").checked;
@@ -19,6 +22,12 @@ function updateSettingsScreen() {
 	} else {
 		document.getElementById("proxy-user").parentNode.classList.add("disabled");
 		document.getElementById("proxy-port").parentNode.classList.add("disabled");
+	}
+
+	if(document.getElementById("audio-volume").checked) {
+		document.getElementById("audio-volume-val").parentNode.classList.remove("disabled");
+	} else {
+		document.getElementById("audio-volume-val").parentNode.classList.add("disabled");
 	}
 }
 
