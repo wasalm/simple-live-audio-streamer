2608200e73aa83e0cf3c32f652f398b910656707
Andries Salm <info@andries-salm.com>
2020-03-08 17:38:03 +0000

    Reenstated share.html, also fixed issue play btn after reloading page

--- experiments/site/assets/scripts/audio-player.js
+++ experiments/site/assets/scripts/audio-player.js
@@ -15,20 +15,34 @@ else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 // play button event listenter
 pButton.addEventListener("click", play);
 
+
+window.addEventListener("pageshow", () => {
+  updateBtn();
+});
+
+
+function updateBtn() {
+  // start music
+  if (music.paused) {
+    pButton.className = "";
+    pButton.className = "fas fa-play";
+  } else { // pause music
+    // remove pause, add play
+    pButton.className = "";
+    pButton.className = "fas fa-pause";
+  }  
+}
+
 //Play and Pause
 function play() {
   // start music
   if (music.paused) {
     music.play();
-    // remove play, add pause
-    pButton.className = "";
-    pButton.className = "fas fa-pause";
   } else { // pause music
     music.pause();
     music.currentTime = 0
-    // remove pause, add play
-    pButton.className = "";
-    pButton.className = "fas fa-play";
   }
+
+  updateBtn();
 }
 
