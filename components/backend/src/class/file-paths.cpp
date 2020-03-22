58541404bfd4f60aee73f751ef1f1086dee1ae65
Andries Salm <info@andries-salm.com>
2020-03-22 20:38:15 +0100

    Get current filepath (NOT FINISHED YET!)

--- components/backend/src/class/file-paths.cpp
+++ components/backend/src/class/file-paths.cpp
@@ -8,25 +8,52 @@
 
 #include "file-paths.hpp"
 #include <filesystem>
+#include <mach-o/dyld.h>
 
 void FilePaths::generatePaths() {
     //TODO
     //TODO url encode 
     
-    config =  "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/experiments/config.json";
-    
-    frontEnd = "file:///Users/andries/Development/Git/Grace%20London/simple-live-audio-streamer/components/interface/dist/index.html";
+    char pathStr[1024];
+    uint32_t size = sizeof(pathStr);
+    if (_NSGetExecutablePath(pathStr, &size) != 0) {
+        printf("Path buffer too small; need size %u\n", size);
+        exit(0);
+    }
+        
+    std::filesystem::path path(pathStr);
+    std::string base = path.remove_filename();
 
-    
-    ffmpeg = "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/components/binaries/ffmpeg";
-    lighttpd = "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/components/binaries/lighttpd";
+    // std::cout << path.remove_filename() << std::endl;
+
+
+
+    config = base + "../Resources/config.json";
+    frontEnd = "file://" + base + "../Resources/site/index.html";
+    ffmpeg = base + "ffmpeg";
+    lighttpd = base + "lighttpd";
+    site = "file://" + base + "../Resources/site/index.html";
+
+
+
+
+
+
+
+
+
+    config =  "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/experiments/config.json";
+    frontEnd = "file:///Users/andries/Development/Git/Grace%20London/simple-live-audio-streamer/components/interface/dist/index.html";
     
 	site = "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/components/site/dist/";
+
+
 
     temp = "/Users/andries/Development/Git/Grace London/simple-live-audio-streamer/temp";
     temp += "/simple-livestream-temp";
 
     // readlink /proc/self/exe
+    //std::filesystem::temp_directory_path
 
     /*
      * Create temporary folder and files
